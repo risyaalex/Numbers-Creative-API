a57b102762d357df33068148c09d97419b4e2363
Oleksandra Antonova <werba2010@gmail.com>
2023-08-17 12:39:54 -0400

    adding function fetchData

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
-const url = 'https://numbersapi.p.rapidapi.com/6/21/date?fragment=true&json=true';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '5716441854msh420403d54a0ef24p1e4bcdjsn4d01a8c6b8cd',
-		'X-RapidAPI-Host': 'numbersapi.p.rapidapi.com'
-	}
-};
+const dayInput = document.getElementById('day-input');
+const monthInput = document.getElementById('month-input');
+const weatherInfo = document.getElementById('date-info');
+const searchButton = document.getElementById('search-button');
 
-try {
-	const response = await fetch(url, options);
+let data = null;
+let isLoading = false;
+
+// fetch Data
+async function fetchData(day, month) {
+
+	const url = `https://numbersapi.p.rapidapi.com/${day}/${month}/date?fragment=true&json=true`;
+	console.log("fetchData:", url);
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '5716441854msh420403d54a0ef24p1e4bcdjsn4d01a8c6b8cd',
+			'X-RapidAPI-Host': 'numbersapi.p.rapidapi.com'
+		}
+	};
+
+    try {
+    isLoading = true;
+    const response = await fetch(url, options);
 	const result = await response.text();
 	console.log(result);
-} catch (error) {
-	console.error(error);
+        if (!response.ok) {
+        data = null;
+        throw new Error("Network response was not ok"); 
+    }
+    
+    data = await response.json();
+    console.log("Loading data...");
+    console.log("FETCHING DATA...");
+  } catch (error) {
+    console.error(
+      "There was a problem with the fetch operation:",
+      error.message
+    );
+  } finally {
+    isLoading = false;
+    // fetchNumberData(data);
+    // console.log("fetchData:", data);
+    console.log("END OF FETCHING DATA...");
+  }
 }
+
+fetchData(1, 12)
